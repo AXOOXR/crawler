86311ee70b21c6ec38582bdca00adec52db481fb
Ali <128279347+AXOOXR@users.noreply.github.com>
2025-07-20 14:55:23 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-service = Service(executable_path='/usr/local/bin/chromedriver')
-driver = webdriver.Chrome(service=service)
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://www.google.com")
+print(driver.title)
+driver.quit()
