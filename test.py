b7d6a1853dbed43751aebc01113fdf683aee987b
Ali <zaliali1100@gmail.com>
2025-07-20 14:45:07 +0330

    .

---
+++ test.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path='/usr/local/bin/chromedriver')
+driver = webdriver.Chrome(service=service, options=options)
